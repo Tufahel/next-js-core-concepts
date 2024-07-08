ab668690917d771a816427b3adcc265c31acafe6
Tufahel Ahmed <tufahel96@gmail.com>
2024-07-08 19:31:13 +0600

    optimize image and fonts in next

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import nextImage from "../../assets/next js image.png";
 import React from "react";
 
 const GalleryPage = () => {
@@ -7,15 +8,23 @@ const GalleryPage = () => {
       <h1 className="text-4xl text-center">Image Optimization</h1>
       <h2 className="text-center text-2xl">Regular Image Tag</h2>
       <img
-        src="https://miro.medium.com/v2/resize:fit:828/format:webp/0*9ToWmeRH2_mgrDss"
+        src="https://global.discourse-cdn.com/auth0/original/2X/a/ae35edce19e64c53e5d455b22e8a2c82d093d4c9.png"
         alt=""
         width={500}
         height={500}
         className="mx-auto"
       />
-      <h2 className="text-center text-2xl">Next Js Image Tag</h2>
+      <h2 className="text-center text-2xl">Next Js Image Component</h2>
       <Image
-        src="https://miro.medium.com/v2/resize:fit:828/format:webp/0*9ToWmeRH2_mgrDss"
+        src="https://global.discourse-cdn.com/auth0/original/2X/a/ae35edce19e64c53e5d455b22e8a2c82d093d4c9.png"
+        alt="next-image"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+      <h2 className="text-center text-2xl">Local Image Component</h2>
+      <Image
+        src={nextImage}
         alt="next-image"
         width={500}
         height={500}
